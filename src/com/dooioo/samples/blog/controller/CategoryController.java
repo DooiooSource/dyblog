24a4087e6e4fe71e5304a18bb988e23d846f9123
chopperkuang <chopperkuang@gmail.com>
2013-06-07 15:20:36 +0800

    init project

---
+++ src/com/dooioo/samples/blog/controller/CategoryController.java
@@ -0,0 +1,22 @@
+package com.dooioo.samples.blog.controller;
+
+import com.dooioo.web.controller.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kuang
+ * Date: 12-8-14
+ * Time: 下午5:57
+ */
+@Controller
+@RequestMapping(value = "/category")
+public class CategoryController extends BaseController {
+
+}
